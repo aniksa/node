cd5fa6637acab4ce655a7eb20a7779bfc1e01976
aniksa <aniksa@gmail.com>
2018-05-27 19:31:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,16 @@ var app = express();
 var url = require('url');
 var cors = require('cors');
 var db = require('./handleDB');
-app.set('views',__dirname + '/views');
-//app.set('view engine', 'html');
-//app.engine('html', require('ejs').renderFile);
 app.use(cors());
 app.all('*', function (req, res) {
     var path = url.parse(req.url).pathname;
     var status = url.parse(req.url, true).query;
-    if(path=="/")
+    if(path == "/")
     {
-        db.add_status(status, res);
+        db.list(status, res);
     }
-    else  if (path == "/add") {
-        db.add_status(status, res);
+    else  if (path == "/list") {
+        db.list(status, res);
     }
 });
 app.listen(3000, function () {
